ffb8935b8053d57952f2b460b6d8fa4bcbf8baff
Rn9303 <rvn9303@nyu.edu>
2023-05-08 22:31:45 -0400

    Added CORS stuff

--- backend.py
+++ backend.py
@@ -4,12 +4,14 @@ from sentence_transformers import SentenceTransformer
 
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 from tensorflow import keras
 import numpy as np
 
 
 app = Flask(__name__)
+#CORS(app)
 api = Api(app)
 
 # embedding model
